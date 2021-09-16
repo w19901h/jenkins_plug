8bd67f705cfc487fd838de3ed0d53348bd0ef49a
wang_hao6 <wang_hao6@hoperun.com>
2021-09-16 19:21:57 +0800

    20210916

---
+++ jenkins_plug/src/main/java/com/it/plug/jenkins/jenkins_plug/JenkinsPlugApplication.java
@@ -0,0 +1,13 @@
+package com.it.plug.jenkins.jenkins_plug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPlugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsPlugApplication.class, args);
+    }
+
+}
